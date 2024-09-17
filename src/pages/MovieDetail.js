13dc3de3f290ef60c36cffc049dea8e90ee87174
AmrMaree <amr.maree14@gmail.com>
2024-09-17 17:58:27 +0200

    edited readme file

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -13,7 +13,7 @@ export const MovieDetail = () => {
 
     useEffect(() => {
         async function fetchMovie() {
-            const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=b80d59c33d6d57ed9c7e3713f91c188a`);
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${process.env.REACT_APP_API_KEY}`);
             const json = await response.json()
             setMovie(json);
             console.log(json);
