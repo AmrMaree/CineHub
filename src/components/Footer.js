fa738ba8e8b39d79a41578a17578ad23d4042a76
AmrMaree <amr.maree14@gmail.com>
2024-09-17 16:07:02 +0200

    final commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="bg-white dark:bg-gray-900 border-t-2 border-gray-200 dark:border-t-1 dark:border-gray-900">
             <div className="w-full mx-auto p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link href="/" className="hover:underline">CineHub™</Link>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">CineHub™</Link>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
